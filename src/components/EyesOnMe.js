5633529598911d1a61eb8cc766aab16b00a439a0
Joe Dietrich <joe@joedietrich.net>
2021-08-29 22:40:28 -0400

    commit tree 0500ef9854599b1385c3392f91540b550318ce13

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function handleFocus(e) {
+  console.log('Good!');
+}
+function handleBlur(e) {
+  console.log('Hey! Eyes on me!')
+}
+
+export default function EyesOnMe() {
+  return <button
+    onFocus={handleFocus}
+    onBlur={handleBlur}
+  >Eyes on me</button>
+}
